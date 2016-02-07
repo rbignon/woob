4cf535185a96117516cf885fdd6952dcfebd02b1
Romain Bignon <romain@symlink.me>
2016-02-07 14:34:59 +0100

    from bnporc: hellobank

---
+++ modules/hellobank/module.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2012-2020  Budget Insight
+#
+# This file is part of a weboob module.
+#
+# This weboob module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This weboob module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
+
+
+from weboob.capabilities.bank import CapBank, AccountNotFound
+from weboob.tools.backend import Module, BackendConfig
+from weboob.tools.value import ValueBackendPassword
+
+from .pp.browser import HelloBank
+
+
+__all__ = ['HelloBankModule']
+
+
+class HelloBankModule(Module, CapBank):
+    NAME = 'hellobank'
+    MAINTAINER = u'Romain Bignon'
+    EMAIL = 'romain@weboob.org'
+    VERSION = '1.1'
+    LICENSE = 'AGPLv3+'
+    DESCRIPTION = 'BNP Paribas'
+    CONFIG = BackendConfig(
+        ValueBackendPassword('login',      label=u'Numéro client', masked=False),
+        ValueBackendPassword('password',   label=u'Code secret', regexp='^(\d{6}|)$'))
+    BROWSER = HelloBank
+
+    def create_default_browser(self):
+        return self.create_browser(self.config['login'].get(),
+                                   self.config['password'].get())
+
+    def iter_accounts(self):
+        return self.browser.get_accounts_list()
+
+    def get_account(self, _id):
+        account = self.browser.get_account(_id)
+        if account:
+            return account
+        else:
+            raise AccountNotFound()
+
+    def iter_history(self, account):
+        return self.browser.iter_history(account)
+
+    def iter_coming(self, account):
+        return self.browser.iter_coming_operations(account)
+
+    def iter_investment(self, account):
+        return self.browser.iter_investment(account)
