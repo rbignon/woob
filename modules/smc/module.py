fb2fae1ebc5482518b774cb029536fdc7724a902
Edouard Lambert <elambert@budget-insight.com>
2017-02-01 14:19:43 +0100

    creditdunord : use AbstractModule and adding capprofile

--- modules/smc/module.py
+++ modules/smc/module.py
@@ -18,8 +18,9 @@
 # along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
 
 
-from weboob.capabilities.bank import CapBank, AccountNotFound
-from weboob.tools.backend import Module, BackendConfig
+from weboob.capabilities.bank import CapBank
+from weboob.capabilities.profile import CapProfile
+from weboob.tools.backend import AbstractModule, BackendConfig
 from weboob.tools.value import ValueBackendPassword
 
 from .browser import CreditDuNordBrowser
@@ -28,7 +29,7 @@ from .browser import CreditDuNordBrowser
 __all__ = ['CreditDuNordModule']
 
 
-class CreditDuNordModule(Module, CapBank):
+class CreditDuNordModule(AbstractModule, CapBank, CapProfile):
     NAME = 'smc'
     MAINTAINER = u'Romain Bignon'
     EMAIL = 'romain@weboob.org'
@@ -38,34 +39,7 @@ class CreditDuNordModule(Module, CapBank):
     CONFIG = BackendConfig(ValueBackendPassword('login',    label='Identifiant', masked=False),
                            ValueBackendPassword('password', label='Code confidentiel'))
     BROWSER = CreditDuNordBrowser
+    PARENT = 'creditdunord'
 
     def create_default_browser(self):
         return self.create_browser(self.weboob, 'www.smc.fr', self.config['login'].get(), self.config['password'].get())
-
-    def iter_accounts(self):
-        for account in self.browser.get_accounts_list():
-            account._bisoftcap = {'cb': {'softcap_day':50,'day_for_softcap':25}, 'deferred_cb': {'softcap_day':50,'day_for_softcap':25}}
-            yield account
-
-    def get_account(self, _id):
-        account = self.browser.get_account(_id)
-        if account:
-            return account
-        else:
-            raise AccountNotFound()
-
-    def iter_history(self, account):
-        account = self.browser.get_account(account.id)
-        for tr in self.browser.get_history(account):
-            if not tr._is_coming:
-                yield tr
-
-    def iter_coming(self, account):
-        account = self.browser.get_account(account.id)
-        for tr in self.browser.get_history(account, coming=True):
-            if tr._is_coming:
-                yield tr
-
-    def iter_investment(self, account):
-        account = self.browser.get_account(account.id)
-        return self.browser.get_investment(account)
