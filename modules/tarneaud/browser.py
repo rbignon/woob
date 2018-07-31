e794e100b788b63ae8a77a02edf2e2961e7a62b2
Simon Rochwerg <simon.rochwerg@budget-insight.com>
2018-07-31 14:55:04 +0200

    [tarneaud] now abstract of kolb

--- modules/tarneaud/browser.py
+++ modules/tarneaud/browser.py
@@ -17,7 +17,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
 
+from weboob.browser import AbstractBrowser
 
-from .module import KolbModule
 
-__all__ = ['KolbModule']
+class KolbBrowser(AbstractBrowser):
+    BASEURL = 'https://www.tarneaud.fr/'
+    PARENT = 'kolb'
