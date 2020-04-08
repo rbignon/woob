d175ff2d86b9ee5defea26301cae8ce2499d93ff
Vincent Ardisson <vardisson@budget-insight.com>
2020-04-08 16:07:29 +0200

    [hellobank] bump to 2.1

--- modules/hellobank/module.py
+++ modules/hellobank/module.py
@@ -47,7 +47,7 @@ class HelloBankModule(Module, CapBankWealth, CapBankTransferAddRecipient, CapPro
     NAME = 'hellobank'
     MAINTAINER = u'Romain Bignon'
     EMAIL = 'romain@weboob.org'
-    VERSION = '1.1'
+    VERSION = '2.1'
     LICENSE = 'AGPLv3+'
     DESCRIPTION = 'BNP Paribas'
     CONFIG = BackendConfig(
