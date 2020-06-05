109d369753672a65bc8e0be4e751debe6317472b
Vincent A <dev@indigo.re>
2020-06-05 19:25:57 +0200

    weboob-config: fix python3 compatibility when no HTTP repository
    When no HTTP repository, there might be no version number for modules, so the
    value is None, which can't be compared to anything else.

--- weboob/applications/weboobcfg/weboobcfg.py
+++ weboob/applications/weboobcfg/weboobcfg.py
@@ -269,7 +269,11 @@ class WeboobCfg(ReplApplication):
         module_info['license'] = minfo.license
         module_info['description'] = minfo.description
         module_info['capabilities'] = minfo.capabilities
-        module_info['installed'] = '%s%s' % (('yes' if module else 'no'), ' (new version available)' if self.weboob.repositories.versions.get(minfo.name) > minfo.version else '')
+        repo_ver = self.weboob.repositories.versions.get(minfo.name)
+        module_info['installed'] = '%s%s' % (
+            'yes' if module else 'no',
+            ' (new version available)' if repo_ver and repo_ver > minfo.version else ''
+        )
         module_info['location'] = '%s' % (minfo.url or os.path.join(minfo.path, minfo.name))
         if module:
             module_info['config'] = {}
