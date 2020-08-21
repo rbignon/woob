477803921b44db6dc2ae77aa40f6cfd3ae30e95d
Vincent Ardisson <vardisson@budget-insight.com>
2020-08-21 15:38:00 +0200

    weboob.capabilities.bank: split file into a folder with submodules
    Introduce:
    - base: for CapBank, Account, Transaction and standard classes
    - rate: for CapCurrencyRate
    - transfer: for CapTransfer and related classes
    - wealth: for CapBankWealth and related classes
    
    To keep compatibility with existing code using base imports, bank/__init__.py
    will import symbols from the subfiles.

---
+++ weboob/capabilities/bank/__init__.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2010-2016 Romain Bignon
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+from .transfer import (
+    EmitterNumberType,
+    Emitter,
+    TransferFrequency,
+    TransferDateType,
+    TransferStatus,
+    TransferError,
+    TransferBankError,
+    TransferInvalidEmitter,
+    TransferInvalidRecipient,
+    TransferInvalidLabel,
+    TransferInvalidAmount,
+    TransferInvalidCurrency,
+    TransferInsufficientFunds,
+    TransferInvalidDate,
+    TransferInvalidOTP,
+    TransferCancelledByUser,
+    BeneficiaryType,
+    RecipientNotFound,
+    RecipientInvalidLabel,
+    Recipient,
+    Transfer,
+    TransferStep,
+    AddRecipientError,
+    AddRecipientBankError,
+    AddRecipientTimeout,
+    AddRecipientStep,
+    RecipientInvalidOTP,
+    RecipientInvalidIban,
+    CapTransfer,
+    CapBankTransfer,
+    CapBankTransferAddRecipient,
+)
+from .base import (
+    AccountNotFound,
+    AccountType,
+    TransactionType,
+    AccountOwnerType,
+    Account,
+    Loan,
+    Transaction,
+    AccountOwnership,
+    CapBank,
+)
+from .rate import Rate, CapCurrencyRate
+from .wealth import (
+    Investment,
+    Per,
+    CapBankWealth,
+)
+
+
+__all__ = [
+    'EmitterNumberType',
+    'Emitter',
+    'TransferFrequency',
+    'TransferDateType',
+    'TransferStatus',
+    'TransferError',
+    'TransferBankError',
+    'TransferInvalidEmitter',
+    'TransferInvalidRecipient',
+    'TransferInvalidLabel',
+    'TransferInvalidAmount',
+    'TransferInvalidCurrency',
+    'TransferInsufficientFunds',
+    'TransferInvalidDate',
+    'TransferInvalidOTP',
+    'TransferCancelledByUser',
+    'BeneficiaryType',
+    'RecipientNotFound',
+    'RecipientInvalidLabel',
+    'Recipient',
+    'Transfer',
+    'TransferStep',
+    'AddRecipientError',
+    'AddRecipientBankError',
+    'AddRecipientTimeout',
+    'AddRecipientStep',
+    'RecipientInvalidOTP',
+    'RecipientInvalidIban',
+    'CapTransfer',
+    'CapBankTransfer',
+    'CapBankTransferAddRecipient',
+    'AccountNotFound',
+    'AccountType',
+    'TransactionType',
+    'AccountOwnerType',
+    'Account',
+    'Loan',
+    'Transaction',
+    'AccountOwnership',
+    'CapBank',
+    'Rate',
+    'CapCurrencyRate',
+    'Investment',
+    'Per',
+    'CapBankWealth',
+]
