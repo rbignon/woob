ab13e1f0d440fd56d7e92423409e1f52f4ce41ff
Vincent A <dev@indigo.re>
2020-10-31 16:02:37 +0100

    [instagram] some entries contain several pics, yield them too
    When an entry has multiple pics, they all have the same date and text.
    Only the first pic has a thumbnail, copy the thumbnail though it's
    wrong.

--- modules/instagram/pages.py
+++ modules/instagram/pages.py
@@ -23,13 +23,13 @@ import re
 
 from weboob.browser.elements import method, DictElement, ItemElement
 from weboob.browser.filters.json import Dict
-from weboob.browser.filters.standard import FromTimestamp
+from weboob.browser.filters.standard import FromTimestamp, Env
 from weboob.browser.pages import HTMLPage, JsonPage
 from weboob.capabilities.image import BaseImage, Thumbnail
 from weboob.capabilities.file import LICENSES
 
 
-class image_element(ItemElement):
+class shared_image_element(ItemElement):
     klass = BaseImage
 
     obj_id = Dict('node/id')
@@ -38,12 +38,48 @@ class image_element(ItemElement):
     obj_ext = 'jpg'
     obj_license = LICENSES.COPYRIGHT
 
+    obj_url = Dict('node/display_url')
+
+
+class single_element(shared_image_element):
+    # entry with a single pic
+
     obj_date = FromTimestamp(Dict('node/taken_at_timestamp'))
     obj_description = Dict('node/accessibility_caption', default=None)
-    obj_url = Dict('node/display_url')
 
     def obj_thumbnail(self):
         return Thumbnail(Dict('node/thumbnail_src')(self))
+
+
+class env_image_element(shared_image_element):
+    # entry with multiple pics
+    # this is a child node of an entry, it shares some info with its siblings nodes
+
+    obj_date = Env('date')
+    obj_description = Env('description')
+    obj_thumbnail = Env('thumbnail')
+
+
+class children_elements(DictElement):
+    item_xpath = 'node/edge_sidecar_to_children/edges'
+
+    item = env_image_element
+
+    def parse(self, el):
+        self.env['date'] = FromTimestamp(Dict('node/taken_at_timestamp'))(self)
+        self.env['description'] = Dict('node/accessibility_caption', default=None)(self)
+        self.env['thumbnail'] = Thumbnail(Dict('node/thumbnail_src')(self))
+
+
+class single_or_multiple_element(DictElement):
+    # for each entry, there can be:
+    # - 1 single picture (single_element)
+    # - or several pictures (children edges)
+
+    def __iter__(self):
+        if 'edge_sidecar_to_children' in self.el['node']:
+            return iter(children_elements(self.page, self, self.el))
+        return iter(single_element(self.page, self, self.el))
 
 
 class ListPageMixin:
@@ -75,7 +111,7 @@ class HomePage(ListPageMixin, JsonPage):
     class iter_images(DictElement):
         item_xpath = 'entry_data/ProfilePage/0/graphql/user/edge_owner_to_timeline_media/edges'
 
-        item = image_element
+        item = single_or_multiple_element
 
 
 class OtherPage(ListPageMixin, JsonPage):
@@ -87,4 +123,4 @@ class OtherPage(ListPageMixin, JsonPage):
     class iter_images(DictElement):
         item_xpath = 'data/user/edge_owner_to_timeline_media/edges'
 
-        item = image_element
+        item = single_or_multiple_element
