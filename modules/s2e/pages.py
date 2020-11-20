983d3e0f750dedac3300663d1d5105078e2fb9fe
Quentin Defenouillere <quentin.defenouillere@budget-insight.com>
2020-11-20 16:32:57 +0100

    [s2e] Ignore deficient url for Investment details
    We cannot fetch details for Investments with a "https://epargneentreprise.axa.fr" URL
    because it leads to a Connectionerror even in on the website.

--- modules/s2e/pages.py
+++ modules/s2e/pages.py
@@ -551,6 +551,8 @@ class ItemInvestment(ItemElement):
                     # The AXA website displays performance graphs but everything is calculated using JS scripts.
                     # There is an API but it only contains risk data and performances per year, not 1-3-5 years.
                     'https://epargne-salariale.axa-im.fr/fr/',
+                    # This URL leads to a connection error even on the website
+                    'https://epargneentreprise.axa.fr',
                     # Redirection to the Rothschild Gestion website, which doesn't exist anymore...
                     'https://www.rothschildgestion.com',
                     # URL to the Morningstar website does not contain any useful information
