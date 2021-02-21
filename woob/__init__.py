76f38060c5b72109eed93a91afcbde0c05de25a9
Vincent A <dev@indigo.re>
2021-02-21 11:11:42 +0100

    woob: introduce package, importing weboob for now
    After years of controversy around the project name and content because of
    poor-taste jokes, the project and its apps will be renamed and the
    offensive content be removed. We are sorry for the past offensive content
    and we apologize for any harm that has been caused by it.
    
    In the next release, everything will be in the woob namespace. In the
    meantime, we keep both packages, so apps can test the transition.
    This is the first step of many towards full renaming of the project.

--- woob/__init__.py
+++ woob/__init__.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# Copyright(C) 2010-2013 Christophe Benz
+# Copyright(C) 2010-2021 Romain Bignon
 #
-# This file is part of weboob.
+# This file is part of woob.
 #
-# weboob is free software: you can redistribute it and/or modify
+# woob is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# weboob is distributed in the hope that it will be useful,
+# woob is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+# along with woob. If not, see <http://www.gnu.org/licenses/>.
+
+import weboob
 
 
-from .bcall import CallErrors
-from .ouiboube import Weboob, WebNip  # noqa
-from .woob import Woob, WoobBase
-
-__all__ = ['CallErrors', 'Woob', 'WoobBase']
+__path__ = weboob.__path__
