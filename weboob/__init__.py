a645e2b19e702f5b4c1a9865098321e8a2b92b5f
Romain Bignon <romain@symlink.me>
2021-03-06 13:25:04 +0100

    __copyright__: use datetime to get the current year

--- weboob/__init__.py
+++ weboob/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 import pkgutil
 
 __path__ = pkgutil.extend_path(__path__, __name__)
@@ -7,4 +8,4 @@ __path__ = pkgutil.extend_path(__path__, __name__)
 __title__ = 'woob'
 __version__ = '2.1'
 __author__ = 'The Woob Association'
-__copyright__ = 'Copyright 2012-2019 The Woob Association'
+__copyright__ = 'Copyright(C) 2012-%s The Woob Association' % datetime.today().year
