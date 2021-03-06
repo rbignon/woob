3b0c35ca992bfa6830f68fbb1a297a58e3d2567f
Romain Bignon <romain@symlink.me>
2021-03-06 13:24:35 +0100

    woob main: more user-friendly launcher

--- weboob/applications/main/main.py
+++ weboob/applications/main/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright(C) 2009-2017  Romain Bignon
+# Copyright(C) 2009-2021  Romain Bignon
 #
 # This file is part of woob.
 #
@@ -18,35 +18,69 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with woob. If not, see <http://www.gnu.org/licenses/>.
 
-import argparse
+import sys
+import difflib
 import importlib
 import pkgutil
 
+from woob import __name__, __version__, __copyright__
 import woob.applications
-
-
-def list_apps():
-    apps = set()
-    for module in pkgutil.iter_modules(woob.applications.__path__):
-        apps.add(module.name)
-
-    apps.remove("main")
-    return sorted(apps)
-
-
-def run_app(app, args):
-    app_module = importlib.import_module("woob.applications.%s" % app)
-    app_class = getattr(app_module, app_module.__all__[0])
-    app_class.run([app] + args)
 
 
 class WoobMain(object):
     @classmethod
+    def list_apps(cls):
+        apps = set()
+        for module in pkgutil.iter_modules(woob.applications.__path__):
+            apps.add(module.name)
+
+        apps.remove("main")
+        return sorted(apps)
+
+    @classmethod
+    def load_app(cls, app):
+        app_module = importlib.import_module("woob.applications.%s" % app)
+        return getattr(app_module, app_module.__all__[0])
+
+    @classmethod
+    def run_app(cls, app, args):
+        app_class = cls.load_app(app)
+        return app_class.run([app] + args)
+
+    @classmethod
+    def print_list(cls, app_list):
+        print('usage: %s [--version] <command> [<args>]' % sys.argv[0])
+        print()
+        print('Use one of this command:')
+        for app in app_list:
+            app_class = cls.load_app(app)
+            print('   %-15s %s' % (app_class.APPNAME, app_class.SHORT_DESCRIPTION))
+        print()
+        print('For more information about a command, use:')
+        print('   $ man woob-<command>')
+        print('or')
+        print('   $ woob <command> --help')
+
+    @classmethod
+    def print_version(cls):
+        print('%s v%s %s' % (__name__, __version__, __copyright__))
+
+    @classmethod
     def run(cls):
-        app_list = list_apps()
+        app_list = cls.list_apps()
 
-        parser = argparse.ArgumentParser(add_help=False)
-        parser.add_argument("app", choices=app_list)
-        args, rest = parser.parse_known_args()
+        if len(sys.argv) < 2 or sys.argv[1] == '--help':
+            return cls.print_list(app_list)
 
-        run_app(args.app, rest)
+        if sys.argv[1] == '--version':
+            return cls.print_version()
+
+        if sys.argv[1] not in app_list:
+            print("{0}: '{1}' is not a {0} command. See '{0} --help'.".format(sys.argv[0], sys.argv[1]))
+            words = difflib.get_close_matches(sys.argv[1], app_list)
+            if words:
+                print()
+                print('The most similar command is\n\t{0}'.format('\n\t'.join(words)))
+            return 1
+
+        return cls.run_app(sys.argv[1], sys.argv[2:])
