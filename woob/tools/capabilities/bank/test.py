6f1b8f5bde2054a6a5c3124a48d526be2d325d19
Vincent Ardisson <vardisson@budget-insight.com>
2021-03-24 16:49:49 +0100

    weboob.tools.capabilities.bank.test: drop numpy dependency

--- woob/tools/capabilities/bank/test.py
+++ woob/tools/capabilities/bank/test.py
@@ -31,6 +31,15 @@ from woob.tools.date import new_date
 __all__ = ('BankStandardTest',)
 
 
+def sign(n):
+    if n < 0:
+        return -1
+    elif n > 0:
+        return 1
+    else:
+        return 0
+
+
 class BankStandardTest(object):
     """Mixin for simple tests on CapBank backends.
 
