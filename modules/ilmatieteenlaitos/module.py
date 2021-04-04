a41c549206da4cd9b026f4dcf7432ad128231c28
Roger Philibert <roger.philibert@gmail.com>
2021-04-04 12:11:57 +0200

    update copyrights

--- modules/ilmatieteenlaitos/module.py
+++ modules/ilmatieteenlaitos/module.py
@@ -2,20 +2,20 @@
 
 # Copyright(C) 2015 Matthieu Weber
 #
-# This file is part of a weboob module.
+# This file is part of a woob module.
 #
-# This weboob module is free software: you can redistribute it and/or modify
+# This woob module is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This weboob module is distributed in the hope that it will be useful,
+# This woob module is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
+# along with this woob module. If not, see <http://www.gnu.org/licenses/>.
 
 
 from weboob.capabilities.weather import CapWeather, CityNotFound
