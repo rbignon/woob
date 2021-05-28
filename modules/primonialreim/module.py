3b5f6a00af7a4f0a9acd494eaa86157355c6a51e
Vincent A <dev@indigo.re>
2021-05-28 16:14:27 +0200

    [primonialreim] fix woob references

--- modules/primonialreim/module.py
+++ modules/primonialreim/module.py
@@ -2,30 +2,30 @@
 
 # Copyright(C) 2019      Vincent A
 #
-# This file is part of a weboob module.
+# This file is part of a woob module.
 #
-# This weboob module is free software: you can redistribute it and/or modify
+# This woob module is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This weboob module is distributed in the hope that it will be useful,
+# This woob module is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
+# along with this woob module. If not, see <http://www.gnu.org/licenses/>.
 
 # flake8: compatible
 
 from __future__ import unicode_literals
 
-from weboob.tools.backend import Module, BackendConfig
-from weboob.tools.value import Value, ValueBackendPassword
-from weboob.capabilities.base import find_object
-from weboob.capabilities.bank import CapBank, Account
-from weboob.capabilities.bill import (
+from woob.tools.backend import Module, BackendConfig
+from woob.tools.value import Value, ValueBackendPassword
+from woob.capabilities.base import find_object
+from woob.capabilities.bank import CapBank, Account
+from woob.capabilities.bill import (
     CapDocument, Subscription, SubscriptionNotFound, DocumentNotFound,
 )
 
