b99e3142c12d8dda5c95d698582cf12106ed3a74
Nicolas Vergnac <nicolas.vergnac@budget-insight.com>
2021-06-01 16:03:53 +0200

    [bnporc] typing per account for bnporc
    new PER account to be typed starting with 'BNPP' instead of 'BNP'

--- modules/bnporc/pp/pages.py
+++ modules/bnporc/pp/pages.py
@@ -251,6 +251,7 @@ class AccountsPage(BNPPage):
                     'PEA Titres': Account.TYPE_PEA,
                     'PEL': Account.TYPE_SAVINGS,
                     'BNP Paribas Multiplacements PER': Account.TYPE_PER,
+                    'BNPP Paribas Multiplacements PER': Account.TYPE_PER,
                     'BNPP MP PERP': Account.TYPE_PERP,
                     'Plan Epargne Retraite Particulier': Account.TYPE_PERP,
                     'Crédit immobilier': Account.TYPE_MORTGAGE,
