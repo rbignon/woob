e952656dd9ddb8d80f8c2df7e15735d239e7119d
Damien Mat <damien.mat@budget-insight.com>
2021-06-30 11:01:08 +0200

    [bred] add AccountNotFound in iter_transfer_recipients
    This would cause an issue when getting an
    account from openapi but that doesn't exist
    in directaccess.

--- modules/bred/module.py
+++ modules/bred/module.py
@@ -96,10 +96,10 @@ class BredModule(Module, CapBankWealth, CapProfile, CapBankTransferAddRecipient)
 
     def iter_transfer_recipients(self, account):
         if not isinstance(account, Account):
-            account = find_object(self.iter_accounts(), id=account)
+            account = find_object(self.iter_accounts(), id=account, error=AccountNotFound)
         elif not hasattr(account, '_univers'):
             # We need a Bred filled Account to know the "univers" associated with the account
-            account = find_object(self.iter_accounts(), id=account.id)
+            account = find_object(self.iter_accounts(), id=account.id, error=AccountNotFound)
 
         return self.browser.iter_transfer_recipients(account)
 
