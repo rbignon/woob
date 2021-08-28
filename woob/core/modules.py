rbignon/woob

commit bad9e4b43af6718b4bb5602c442b97f893f38aad

core: rely on namespace packages for importing modules
Rationale
---------

This has the advantage that modules can be imported just like any
standard Python module.
AbstractModule/Browser/Page will simply become useless over time.
Modules can be migrated to regular imports at different times.

Transition
----------

$WEBOOB_DATADIR/$WEBOOB_VERSION will contain a "weboob_modules" prefix,
instead of directly modules folders.
For users using HTTP repos like updates.weboob.org, transition will be
smooth as "weboob-config update" will just (re)install modules referred
to by $WEBOOB_BACKENDS.

TODO:
* play nice with sources.list
* weboob: remove Abstract*
    * this should be done when all modules are migrated

woob/core/modules.py
@@ -18,8 +18,10 @@
 # along with woob. If not, see <http://www.gnu.org/licenses/>.
 
 import os
-import imp
+import importlib
 import logging
+import pkgutil
+import sys
 
 from woob.tools.backend import Module, BackendConfig
 from woob.tools.compat import basestring
@@ -103,9 +105,8 @@ class ModulesLoader(object):
     Load modules.
     """
 
-    def __init__(self, path, version=None):
+    def __init__(self, version=None):
         self.version = version
-        self.path = path
         self.loaded = {}
         self.logger = getLogger('modules')
 
@@ -118,13 +119,12 @@ class ModulesLoader(object):
         return self.loaded[module_name]
 
     def iter_existing_module_names(self):
-        for name in os.listdir(self.path):
-            try:
-                if '__init__.py' in os.listdir(os.path.join(self.path, name)):
-                    yield name
-            except OSError:
-                # if path/name is not a directory
-                continue
+        try:
+            import woob_modules
+        except ImportError:
+            return []
+
+        return pkgutil.iter_modules(woob_modules.__path__)
 
     def module_exists(self, name):
         for existing_module_name in self.iter_existing_module_names():
@@ -144,15 +144,9 @@ class ModulesLoader(object):
             self.logger.debug('Module "%s" is already loaded from %s' % (module_name, self.loaded[module_name].package.__path__[0]))
             return
 
-        path = self.get_module_path(module_name)
-
         try:
-            fp, pathname, description = imp.find_module(module_name, [path])
-            try:
-                module = LoadedModule(imp.load_module(module_name, fp, pathname, description))
-            finally:
-                if fp:
-                    fp.close()
+            pymodule = importlib.import_module('woob_modules.%s' % module_name)
+            module = LoadedModule(pymodule)
         except Exception as e:
             if logging.root.level <= logging.DEBUG:
                 self.logger.exception(e)
@@ -166,7 +160,14 @@ class ModulesLoader(object):
         self.logger.debug('Loaded module "%s" from %s' % (module_name, module.package.__path__[0]))
 
     def get_module_path(self, module_name):
-        return self.path
+        # FIXME implement or remove
+        # this function isn't used anymore
+        try:
+            import woob_modules
+            assert woob_modules  # please pyflakes
+        except ImportError:
+            return ''
+        return ''
 
 
 class RepositoryModulesLoader(ModulesLoader):
@@ -175,8 +176,9 @@ class RepositoryModulesLoader(ModulesLoader):
     """
 
     def __init__(self, repositories):
-        super(RepositoryModulesLoader, self).__init__(repositories.modules_dir, repositories.version)
+        super(RepositoryModulesLoader, self).__init__(repositories.version)
         self.repositories = repositories
+        sys.path.append(os.path.dirname(repositories.modules_dir))
 
     def iter_existing_module_names(self):
         for name in self.repositories.get_all_modules_info():

setup.cfg
@@ -22,7 +22,7 @@ tests = woob.tools.capabilities.bank.iban,
         woob.browser.tests.xpath_functions
 
 [isort]
-known_first_party = woob, weboob
+known_first_party = woob, weboob, weboob_modules
 line_length = 120
 
 [flake8]

tools/hooks/run-flake8.py
@@ -44,7 +44,7 @@ pkg_resources.working_set.add(d, 'fake_module_all_separate')
 #   But it's a nice visual separator sometimes.
 main([
     '--ignore=E501,W503,E266',
-    '--application-import-names=weboob,woob',
+    '--application-import-names=weboob,woob,weboob_modules',
     '--import-order-style=fake_module_all_separate',
     *map(str, mod['files_to_check'](args)),
 ])

woob/core/repositories.py
@@ -19,7 +19,7 @@
 
 
 from __future__ import print_function
-import imp
+import importlib
 import posixpath
 import shutil
 import re
@@ -305,12 +305,8 @@ class Repository(object):
                 continue
 
             try:
-                fp, pathname, description = imp.find_module(name, [path])
-                try:
-                    module = LoadedModule(imp.load_module(name, fp, pathname, description))
-                finally:
-                    if fp:
-                        fp.close()
+                pymodule = importlib.import_module('woob_modules.%s' % name)
+                module = LoadedModule(pymodule)
             except Exception as e:
                 self.logger.warning('Unable to build module %s: [%s] %s' % (name, type(e).__name__, e))
                 bt = get_backtrace(e)
@@ -445,13 +441,14 @@ https://updates.woob.tech/%(version)s/main/
 # DEVELOPMENT
 # If you want to hack on Woob modules, you may add a
 # reference to sources, for example:
-#file:///home/rom1/src/woob/modules/
+#file:///home/rom1/src/woob/weboob_modules/
 """
 
 
 class Repositories(object):
     SOURCES_LIST = 'sources.list'
     MODULES_DIR = 'modules'
+    MODULES_SUBDIR = 'woob_modules'
     REPOS_DIR = 'repositories'
     KEYRINGS_DIR = 'keyrings'
     ICONS_DIR = 'icons'
@@ -467,13 +464,14 @@ class Repositories(object):
         self.workdir = workdir
         self.datadir = datadir
         self.sources_list = os.path.join(self.workdir, self.SOURCES_LIST)
-        self.modules_dir = os.path.join(self.datadir, self.MODULES_DIR, self.version)
+        self.modules_dir = os.path.join(self.datadir, self.MODULES_DIR, self.version, self.MODULES_SUBDIR)
         self.repos_dir = os.path.join(self.datadir, self.REPOS_DIR)
         self.keyrings_dir = os.path.join(self.datadir, self.KEYRINGS_DIR)
         self.icons_dir = os.path.join(self.datadir, self.ICONS_DIR)
 
         self.create_dir(self.datadir)
         self.create_dir(self.modules_dir)
+        self.create_namespace_package(self.modules_dir)
         self.create_dir(self.repos_dir)
         self.create_dir(self.keyrings_dir)
         self.create_dir(self.icons_dir)
@@ -506,6 +504,13 @@ class Repositories(object):
             os.makedirs(name)
         elif not os.path.isdir(name):
             self.logger.error(u'"%s" is not a directory' % name)
+
+    def create_namespace_package(self, path):
+        pypath = os.path.join(path, '__init__.py')
+        if not os.path.exists(pypath):
+            with open(pypath, 'wt') as fd:
+                print('from pkgutil import extend_path', file=fd)
+                print('__path__ = extend_path(__path__, __name__)', file=fd)
 
     def _extend_module_info(self, repo, info):
         if repo.local:

woob/core/woob.py
@@ -67,17 +67,7 @@ class WoobBase(object):
         self.backend_instances = {}
         self.requests = RequestsManager()
 
-        if modules_path is None:
-            import pkg_resources
-            # Package woob_modules is provided by
-            # https://gitlab.com/woob/modules
-            # and should be pip-installed separately.
-            # Note that Weboob users should rather install Weboob modules
-            # through https://updates.woob.tech/.
-            modules_path = pkg_resources.resource_filename('woob_modules', '')
-
-        if modules_path:
-            self.modules_loader = ModulesLoader(modules_path, self.VERSION)
+        self.modules_loader = ModulesLoader(self.VERSION)
 
         if scheduler is None:
             scheduler = Scheduler()

commit d5dda19d2ba7f4bac33bb83d6cef97cc1833ca92

woob.core: fix iter_existing_module_names which should return names
Regression introduced in bad9e4b43af6718b4bb5602c442b97f893f38aad

woob/core/modules.py
@@ -140,9 +140,10 @@ class ModulesLoader(object):
         try:
             import woob_modules
         except ImportError:
-            return []
+            return
 
-        return pkgutil.iter_modules(woob_modules.__path__)
+        for module in pkgutil.iter_modules(woob_modules.__path__):
+            yield module.name
 
     def module_exists(self, name):
         for existing_module_name in self.iter_existing_module_names():