dd02e58fc68176359a0228b9924ca70b1a907755
Vincent A <dev@indigo.re>
2022-06-07 11:46:40 +0200

    woob.capabilities.profile: more improvements to Person
    - maiden_name is replaced with more neutral birth_name
    - remove non-obvious "dependant" children hint
    - many fields belong to Company, just have a company field

--- woob/capabilities/profile.py
+++ woob/capabilities/profile.py
@@ -17,8 +17,13 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with woob. If not, see <http://www.gnu.org/licenses/>.
 
+import warnings
+
 from .address import PostalAddress, compat_field
-from .base import Capability, BaseObject, DecimalField, StringField, UserError, Field
+from .base import (
+    Capability, BaseObject, DecimalField, StringField, UserError, Field,
+    DeprecatedFieldWarning,
+)
 from .date import DateField
 
 __all__ = ['Profile', 'Person', 'Company', 'CapProfile']
@@ -46,6 +51,15 @@ class Profile(BaseObject):
     country = compat_field('postal_address', 'country')
 
 
+class Company(Profile):
+    """
+    Company.
+    """
+    siren = StringField('French SIREN Number')
+    registration_date = DateField('Registration date')
+    activity_area = StringField('Activity area')
+
+
 class Person(Profile):
     """
     Person.
@@ -56,28 +70,37 @@ class Person(Profile):
     nationality =                 StringField('Nationality of person')
     mobile =                      StringField('Mobile number of person')
     gender =                      StringField('Gender of person')
-    maiden_name =                 StringField('Maiden name')
+    birth_name =                  StringField('Birth name')
     spouse_name =                 StringField('Name of spouse')
-    children =                    DecimalField('Number of dependent children')
+    children =                    DecimalField('Number of children')
     family_situation =            StringField('Family situation')
     matrimonial =                 StringField('Matrimonial status')
     housing_status =              StringField('Housing status')
     job =                         StringField('Profession')
     job_start_date =              DateField('Start date of current job')
-    job_activity_area =           StringField('Activity area of company')
     job_contract_type =           StringField('Contract type of current job')
-    company_name =                StringField('Name of company')
-    company_siren =               StringField('SIREN Number of company')
+    company =                     Field("Company of current job", Company)
     socioprofessional_category =  StringField('Socio-Professional Category')
 
+    job_activity_area = compat_field("company", "activity_area")
+    company_name = compat_field("company", "name")
+    company_siren = compat_field("company", "siren")
 
-class Company(Profile):
-    """
-    Company.
-    """
-    siren =                       StringField('SIREN Number')
-    registration_date  =          DateField('Registration date')
-    activity_area =               StringField('Activity area')
+    @property
+    def maiden_name(self):
+        warnings.warn(
+            'Field is deprecated, use .birth_name field instead.',
+            DeprecatedFieldWarning, stacklevel=3,
+        )
+        return self.birth_name
+
+    @maiden_name.setter
+    def maiden_name(self, value):
+        warnings.warn(
+            'Field is deprecated, use .birth_name field instead.',
+            DeprecatedFieldWarning, stacklevel=3,
+        )
+        self.birth_name = value
 
 
 class CapProfile(Capability):
