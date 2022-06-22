bad8b17d7cba8383abd2e64f8fcb9a832e6f4678
Thomas Touhey <thomas@touhey.fr>
2022-06-22 22:53:26 +0200

    [capabilities/picross] Added picross solver in contrib
    This script is a simple demonstration for the picross
    capability, with the possibility to list a few unsolved
    picross puzzles, and submit an automatically computed
    solution for a given picross puzzle.

---
+++ contrib/picross-solver.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+
+# flake8: compatible
+
+# Copyright(C) 2022      Thomas Touhey <thomas@touhey.fr>
+#
+# This file is part of woob.
+#
+# woob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# woob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with woob. If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+
+from woob.capabilities.picross import (
+    PicrossNotFound, PicrossSolution, PicrossSolutionKind, PicrossSolvedStatus,
+)
+from woob.core.woob import Woob
+
+
+class PicrossSolver:
+    @staticmethod
+    def combinations(fmt, length):
+        """ Réalise des combinaisons. """
+
+        min_length = sum(fmt) + len(fmt) - 1
+        if min_length > length:
+            return []
+
+        def combinations_recur(fmt, spaces):
+            if not fmt:
+                yield ' ' * spaces
+                return
+
+            fst, *rest = fmt
+            for current_spaces in range(0, spaces + 1):
+                for combination in combinations_recur(
+                    rest,
+                    spaces - current_spaces,
+                ):
+                    yield (
+                        ' ' * current_spaces + 'x' * fst
+                        + (' ' if rest else '') + combination
+                    )
+
+        return list(combinations_recur(fmt, length - min_length))
+
+    @staticmethod
+    def precise(combinations, current_row):
+        for i in range(len(combinations) - 1, -1, -1):
+            incompatible = False
+            for cc, cx in zip(combinations[i], current_row):
+                if cx != '?' and cx != cc:
+                    incompatible = True
+                    break
+
+            if incompatible:
+                del combinations[i]
+
+        return combinations
+
+    @staticmethod
+    def intersection(combinations):
+        def intersection_inner(combinations):
+            for s in zip(*combinations):
+                if all(c == s[0] for c in s[1:]):
+                    yield s[0]
+                else:
+                    yield '?'
+
+        return ''.join(intersection_inner(combinations))
+
+    def solve(self, puzzle):
+        """
+        Solve a given picross puzzle.
+
+        Returns a PicrossSolution.
+        """
+
+        w, h = len(puzzle.columns), len(puzzle.lines)
+        ye = puzzle.columns
+        xe = puzzle.lines
+
+        yc = [self.combinations(fmt, h) for fmt in ye]
+        xc = [self.combinations(fmt, w) for fmt in xe]
+
+        current = [self.intersection(c) for c in xc]
+
+        while True:
+            no_unknown = True
+
+            for x in range(w):
+                if len(yc) == 1:
+                    continue
+
+                current_column = ''.join(s[x] for s in current)
+                yc[x] = self.precise(yc[x], current_column)
+                new_column = self.intersection(yc[x])
+                if '?' in new_column:
+                    no_unknown = False
+
+                for y, c in enumerate(new_column):
+                    s = current[y]
+                    current[y] = s[:x] + c + s[x + 1:]
+
+            for y in range(h):
+                if len(xc) == 1:
+                    continue
+
+                current_row = current[y]
+                xc[y] = self.precise(xc[y], current_row)
+                new_row = self.intersection(xc[y])
+                if '?' in new_row:
+                    no_unknown = False
+
+                current[y] = new_row
+
+            if no_unknown:
+                break
+
+        # Make the solution out of the current state.
+
+        solution = PicrossSolution()
+        solution.kind = PicrossSolutionKind.PLACEMENTS
+        solution.lines = current
+
+        return solution
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Solve some picrosses using Woob!',
+    )
+    parser.add_argument('-b', dest='backend', required=True)
+    subcommands = parser.add_subparsers(dest='command', required=True)
+
+    list_unsolved = subcommands.add_parser(
+        'list-unsolved',
+        description='List unsolved picrosses.',
+    )
+    list_unsolved.set_defaults(func=list_unsolved)
+
+    solve = subcommands.add_parser(
+        'solve',
+        description='Solve a given picross',
+    )
+    solve.add_argument('id', help='Identifier of the picross to solve.')
+    solve.set_defaults(func=solve)
+
+    parsed_args = parser.parse_args()
+
+    woob = None
+    try:
+        woob = Woob()
+        woob.load_backends(names=(parsed_args.backend,))
+
+        try:
+            backend = woob.get_backend(parsed_args.backend)
+        except KeyError:
+            print(f'No backend with the name {parsed_args.backend!r}.')
+            exit()
+
+        if parsed_args.command == 'list-unsolved':
+            is_first = True
+            has_stopped_naturally = False
+            for i, picross in enumerate(backend.iter_picross_puzzles(
+                PicrossSolvedStatus.UNSOLVED,
+            )):
+                if is_first:
+                    print('Available picrosses:')
+                    print()
+                    is_first = False
+
+                print(f'{picross.id} - {picross.name} ({picross.variant})')
+
+                if i >= 4:
+                    break
+            else:
+                has_stopped_naturally = True
+
+            if is_first:
+                print('No unsolved picross available. Better luck next time!')
+            elif not has_stopped_naturally:
+                print()
+                print('... and more!')
+        elif parsed_args.command == 'solve':
+            try:
+                picross = backend.get_picross_puzzle(parsed_args.id)
+            except PicrossNotFound:
+                print('Picross not found.')
+            else:
+                solver = PicrossSolver()
+                solution = solver.solve(picross)
+
+                print('Found the following solution:')
+                print()
+
+                for line in solution.lines:
+                    print(' '.join(line))
+
+                print()
+                backend.submit_picross_puzzle_solution(picross, solution)
+                print('Submitted the solution.')
+        else:
+            print(f'Unknown command {parsed_args.command!r}!')
+    finally:
+        if woob is not None:
+            woob.deinit()
