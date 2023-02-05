e47aa46407953a5203ff21252a9f8d694490b329
Romain Bignon <romain@symlink.me>
2023-02-05 19:00:27 +0100

    docs: remove remaining debug prints

--- docs/source/_ext/sorted-toctree.py
+++ docs/source/_ext/sorted-toctree.py
@@ -4,8 +4,6 @@ from sphinx.directives.other import TocTree
 class SortedTocTree(TocTree):
     def parse_content(self, toctree):
         r = super().parse_content(toctree)
-        print('coucou', toctree['entries'])
-        print('coucou2', toctree['includefiles'])
         toctree['entries'] = list(sorted(toctree['entries']))
         toctree['includefiles'] = list(sorted(toctree['includefiles']))
         return r
