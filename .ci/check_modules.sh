3f987d7791d2157042baf8e7a21e4cf913ffd93f
Romain Bignon <romain@symlink.me>
2023-02-27 09:54:43 +0100

    CI: add script check_modules.sh
    This scripts tries to load each module with their own dependences, and
    display errors if any.

---
+++ .ci/check_modules.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Try to load all modules. It may fails if there are missing dependences in the
+# module requirements.txt
+
+errors=''
+
+
+for module in $(ls -d -- modules/*/ | sed 's#modules/\(.*\)/#\1#'); do
+  echo "-------"
+  echo "Module $module"
+
+  # Find a requirements.txt and install dependences. We exclude woob to avoid
+  # installing it from PyPI.
+  module_requirements=modules/$module/requirements.txt
+  tmp_requirements=$(mktemp)
+  output=$(mktemp)
+  [ -f $module_requirements ] && cat $module_requirements | grep -v woob > $tmp_requirements
+  [ -s $tmp_requirements ] && pip install -r $tmp_requirements
+
+  # Python script to load module
+  output=$(python -c "
+from woob.core import WoobBase
+w = WoobBase('modules')
+
+try:
+    w.load_or_install_module('$module')
+except Exception as e:
+    print(e)
+")
+
+  # Uninstall module dependences
+  [ -s $tmp_requirements ] && pip uninstall -y -r $tmp_requirements
+
+  # The python script write something on stdout only if there is an error.
+  if [ ! -z "$output" ]; then
+    echo "Error: $output"
+    errors="$errors$module: $output"$'\n'
+  else
+    echo "OK"
+  fi
+done
+
+echo "-------"
+
+if [ ! -z "$errors" ]; then
+  echo "Errors to load modules:"
+  echo "$errors"
+  exit 1
+fi
