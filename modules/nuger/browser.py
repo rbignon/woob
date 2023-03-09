df17a3105a278d2729cf71db527f6acd11c40cef
Dorian Roly <dorian.roly@budget-insight.com>
2023-03-09 11:25:52 +0100

    [nuger] Add BrowserUnavailable
    Wave 1 of credit du nord and children migration to société générale.

--- modules/nuger/browser.py
+++ modules/nuger/browser.py
@@ -18,8 +18,12 @@
 # along with this woob module. If not, see <http://www.gnu.org/licenses/>.
 
 from woob.browser import AbstractBrowser
+from woob.exceptions import BrowserUnavailable
 
 
 class NugerBrowser(AbstractBrowser):
     BASEURL = 'https://www.banque-nuger.fr'
     PARENT = 'creditdunord'
+
+    def do_login(self):
+        raise BrowserUnavailable()
