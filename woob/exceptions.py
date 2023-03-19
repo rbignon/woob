4ee2c0ae3e190039e764d3fb6a9726bbfe00062e
Romain Bignon <romain@symlink.me>
2023-03-19 14:51:17 +0100

    exceptions: add import to warnings

--- woob/exceptions.py
+++ woob/exceptions.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with woob. If not, see <http://www.gnu.org/licenses/>.
 
+import warnings
 from functools import wraps
 
 from woob.tools.value import Value
