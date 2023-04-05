713bbe9e09cc11230088e6e5dcbdf430443a3c5c
Florian Duguet <florian.duguet@budget-insight.com>
2023-04-05 17:44:13 +0200

    [franceconnect] update Copyright

--- modules/franceconnect/__init__.py
+++ modules/franceconnect/__init__.py
@@ -1,4 +1,4 @@
-# Copyright(C) 2012-2020  Budget Insight
+# Copyright(C) 2012-2020 Powens
 #
 # This file is part of a woob module.
 #
@@ -14,6 +14,8 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this woob module. If not, see <http://www.gnu.org/licenses/>.
+
+# flake8: compatible
 
 from .module import FranceConnectModule
 
