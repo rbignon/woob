74a4ca1bd637dbd442b6128641c4e2ec784544d3
Goulven Riou <goulven.riou@budget-insight.com>
2023-06-07 15:27:23 +0200

    [creditmutuel] update xpath for LIAccountsPage go_acount_list
    in certain case the id_form in go_account_list was updated

--- modules/creditmutuel/pages.py
+++ modules/creditmutuel/pages.py
@@ -1892,7 +1892,7 @@ class LIAccountsPage(LoggedPage, HTMLPage):
 
     def go_accounts_list(self):
         form = self.get_form(
-            xpath="//form[@id='C:P14:F' or @id='C:P4:F' or @id='C:P5:F']",
+            xpath="//form[@id='C:P14:F' or @id='C:P4:F' or @id='C:P5:F'or @id='C:P6:F']",
             submit='//input[@name="_FID_GoBusinessSpaceLife"]'
         )
         form.submit()
