082e58e6d0571cc86feba90abef5d77d5afa843c
Gilles Dartiguelongue <eva@gentoo.org>
2023-08-05 18:12:11 +0200

    Add initial test for lcl module

---
+++ modules/lcl/test.py
@@ -0,0 +1,33 @@
+# Copyright(C) 2023      Gilles Dartiguelongue
+#
+# This file is part of a woob module.
+#
+# This woob module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This woob module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this woob module. If not, see <http://www.gnu.org/licenses/>.
+
+from woob.capabilities.bill import Document
+from woob.tools.test import BackendTest
+
+
+class LclTest(BackendTest):
+    MODULE = "lcl"
+
+    # Bank
+
+    def test_lcl(self):
+        accounts = list(self.backend.iter_accounts())
+        self.assertGreater(len(accounts), 0)
+
+        account = accounts.pop()
+        list(self.backend.iter_coming(account))
+        list(self.backend.iter_history(account))
